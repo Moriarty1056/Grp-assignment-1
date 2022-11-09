fe5ecee973ac5e017158bcf2066ac3b4e14bd249
Tanisha Prakash <106842989+Moriarty1056@users.noreply.github.com>
2022-11-10 00:21:50 +0530

    Update Main class.cpp

--- Main class.cpp
+++ Main class.cpp
@@ -1,27 +1,90 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
+// Main parent class
+
 class FestManagement
 {
     public:
     string name;
     string areaofwork;
     string PositionOfResponsibility;
-    static int numofparticipants;
-    static int numofmembers;
+    static int totalmembers;
+
 
     FestManagement()
     {
         cout<<"Welcome to the 25th annuual Tech fest"<<endl;
     }
+    
+    void Totalpeeps()
+    {
+        cout<<totalmembers<<endl;
+    }
 
 };
 
-class CoreCommittee : public FestManagement {};
+    
+
+// Child class-1
+class CoreCommittee : public FestManagement {
+
+    public:
+    int numofcoremembers;
+    CoreCommittee()
+    {
+        totalmembers++;
+        numofcoremembers++;
+    }
+};
+
+// Child class-2
+
+class Participants : public FestManagement {
+
+    public:
+    int numofparticipants;
+   Participants()
+   {
+        numofparticipants++;
+        totalmembers++;
+   }
+};
+
+// Child class-3
+
+class Coordinators : public FestManagement {
+
+    public:
+    int numofcoordinators;
+    Coordinators()
+    {
+        numofcoordinators++;
+        totalmembers++;
+    }
+
+    
+};
+
+// Child class-4
+
+class Workforce : public FestManagement {
+
+    public:
+    int numofworkforce;
+    Workforce()
+    {
+        numofworkforce++;
+        totalmembers++;
+    }
+
+
+};
+
 
 int main()
 {
-    CoreCommittee Vidya;
-
     return 0;
 }
